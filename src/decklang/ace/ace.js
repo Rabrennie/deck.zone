14ecfda63ff03a233b5651fac08fec47c3ea6fed
rabrennie <rennierab@gmail.com>
2016-05-26 19:02:58 +0100

    converted snippets to template string

--- src/decklang/ace/ace.js
+++ src/decklang/ace/ace.js
@@ -12,8 +12,15 @@ const pluginNames = plugins.map(str => {
 require('brace/ext/language_tools.js');
 
 ace.define('ace/snippets/decklang', (require, exports) => {
-  exports.snippetText = "snippet loop \n\
-loop ${1:test}"
+  exports.snippetText = `
+snippet loop
+	loop = <\${1:i} = \${2:1}> to \${3:10}
+	endloop
+snippet loopin
+	loop = <\${1:item}> in { \${2} }
+	endloop`;
+
+  exports.scope = "decklang"
 })
 
 ace.define('ace/mode/decklang', (require, exports) => {
